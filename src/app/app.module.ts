3e3f70b5e7d503b555f1bb5ef83bbd92102ceec2
tatiana <tetiana.lycheva@nuweb.com>
2017-12-15 19:49:06 +0200

    rebuilding project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,59 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {MatDatepickerIntl, MatListModule, MatNativeDateModule} from '@angular/material';
 import { AppComponent } from './app.component';
+import {PanelClassComponent} from "./panel-classes/panel.class.component";
+import {MatToolbarModule} from '@angular/material';
+import {MatIconModule} from '@angular/material';
+import {MatCardModule} from '@angular/material';
+import {ClassService} from "./service/class.service";
+import {ItemClassComponent} from "./ItemClass/item.class.component";
+import {ItemClassesService} from "./service/item.classes.service";
+import {MatFormFieldModule} from '@angular/material';
+import {MatInputModule} from '@angular/material';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {MatTableModule} from '@angular/material';
+import {LessonServise} from "./service/lesson.servise";
+import {MarksForClassService} from "./service/marks.for.class.service";
+import {MatButtonModule} from '@angular/material';
+import {MatDialogModule} from '@angular/material';
+import {AddLesson} from "./add.lesson/add.lesson";
+import {MatMenuModule} from '@angular/material';
+import {SubjectClassService} from "./service/subject.class.service";
+import {MatDatepickerModule} from '@angular/material';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PanelClassComponent,
+    ItemClassComponent,
+    AddLesson
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatListModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatTableModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatCardModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule
+
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    MatDatepickerIntl,
+    MarksForClassService,
+    SubjectClassService,
+    LessonServise,
+    ClassService,
+    ItemClassesService],
+  bootstrap: [AppComponent],
+  entryComponents: [AddLesson]
 })
 export class AppModule { }
