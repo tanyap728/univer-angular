8e1ebbb818a2488882138b2d54c04f6655ac52eb
Gleb Naymitenko <gozz.kesshell@gmail.com>
2017-12-17 23:31:00 +0200

    fixed getters

--- src/app/models/student.model.ts
+++ src/app/models/student.model.ts
@@ -3,4 +3,10 @@ import {UUID} from "angular2-uuid";
 export class Student {
   constructor(private id: UUID, private name: string) {
   }
+  getId() {
+    return this.id;
+  }
+  getName() {
+    return this.name;
+  }
 }
--- src/app/models/subject.model.ts
+++ src/app/models/subject.model.ts
@@ -3,10 +3,10 @@ import { UUID } from 'angular2-uuid';
 export class Subject {
   constructor(private _id: UUID, private _name: string) {
   }
-  get id() {
+  getId() {
     return this._id;
   }
-  get name() {
+  getName() {
     return this._name;
   }
 }
